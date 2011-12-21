d7ffa581ce54c467a115856d71a4192244cf6fb8
Satya <satya@satya-lt.(none)>
2011-12-21 17:57:41 +0530

    modifications half done

--- src/utility.js
+++ src/utility.js
@@ -12,6 +12,45 @@ AR.Utility.scale = {
 	"ordinal" : "ordinal",
 	"log" : "log"
 };
+
+AR.Utility.getKeys = Object.keys || function () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
+        DontEnums = [ 
+            'toString', 'toLocaleString', 'valueOf', 'hasOwnProperty',
+            'isPrototypeOf', 'propertyIsEnumerable', 'constructor'
+        ],
+        DontEnumsLength = DontEnums.length;
+
+    return function (o) {
+        if (typeof o != "object" && typeof o != "function" || o === null)
+            throw new TypeError("Object.keys called on a non-object");
+
+        var result = [];
+        for (var name in o) {
+            if (hasOwnProperty.call(o, name))
+                result.push(name);
+        }
+
+        if (hasDontEnumBug) {
+            for (var i = 0; i < DontEnumsLength; i++) {
+                if (hasOwnProperty.call(o, DontEnums[i]))
+                    result.push(DontEnums[i]);
+            }   
+        }
+
+        return result;
+    };
+};
+
+AR.Utility.isArray= function(a){
+	return (Object.prototype.toString.apply(a) === '[object Array]');
+};
+
+AR.Utility.isNumber = function(value) { 
+	return value.toFixed && value.toExponential; 
+};
+
 AR.Utility.findMax = function(data) {
 	var max = data[0].value;
 	data.forEach(function(data) {
